396988787b915cd05dda5635a8e8534f3ad5f5f7
Lay182 <jinwoo9288@gmail.com>
2024-07-30 23:28:31 +0900

    part3 start2

---
+++ Course1/model/CarDTO.java
@@ -0,0 +1,10 @@
+package Course1.model;
+
+public class CarDTO {
+        public int carSn;
+        public String carName;
+        public int carPrice;
+        public String carOwner;
+        public int carYear;
+        public String carType;
+}
